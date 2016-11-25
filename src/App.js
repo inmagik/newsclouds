da6d3d6076fcbf186f0e14790da10245ebc27a99
lorenzobianchi <lorenzo.bianchi@inmagik.com>
2016-11-25 16:18:21 +0100

    added footer component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { HashRouter, Match } from 'react-router'
 import NewsWordCloudsList from './components/NewsWordCloudsList'
 import Header from './components/Header'
+import Footer from './components/Footer'
 import Project from './components/Project'
 import './App.css';
 
@@ -11,6 +12,7 @@ const App = () => (
       <Header />
       <Match pattern="/project" exactly component={Project} />
       <Match pattern="/" component={NewsWordCloudsList} />
+      <Footer />
     </div>
   </HashRouter>
 )
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router'
 import './Header.css'
+import logo from './NewsWordClouds_logo.png'
 
 const Header = () => (
   <div className="row">
    <div className="Header col-md-8 col-md-offset-2">
-     <h3 className="Header-logo">NewsWordsCloud <i className="Header-icon fa fa-newspaper-o" />|</h3>
-     <ul>
+     <img src={logo} className="logo"/>
+     <ul className="navbar-menu">
      <li><a href="https://github.com/inmagik/newswordclouds-app">Repo</a></li>
      <li><Link to='/project'>Project</Link></li>
      </ul>
