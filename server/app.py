7e8d2e81af202b8b14ca4619a8197685a62aafd2
gffuma <fumagalli.gf@gmail.com>
2016-12-02 01:04:12 +0100

    Serve other static stuff outside static

--- server/app.py
+++ server/app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request, send_from_directory
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/project")
 def index():
     return render_template("index.html")
 
@@ -9,9 +10,14 @@ def index():
 def wordcloud(cloud):
     return render_template("cloud.html", cloud=cloud)
 
+@app.route('/favicon.ico')
+@app.route('/asset-manifest.json')
+def static_from_root():
+    return send_from_directory('', request.path[1:])
+
 @app.errorhandler(404)
 def not_found(e):
     return redirect(url_for("index"))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
