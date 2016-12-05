fac18d9534b8721f535a781b43d6836862a7f8d6
gffuma <fumagalli.gf@gmail.com>
2016-12-05 13:06:44 +0100

    Modal moved by keys

--- src/components/WordCloudModal/WordCloudModal.js
+++ src/components/WordCloudModal/WordCloudModal.js
@@ -1,9 +1,14 @@
 import React, {Component} from 'react'
-import { Link } from 'react-router'
+import { Link, browserHistory } from 'react-router'
 import { formatDate } from '../../util'
 import './WordCloudModal.css'
 import inmagik from '../Layout/Footer/inmagik_circle_shadow.png'
 //import oldpaper from './old_paper.jpg'
+
+// Key codes for moving modal
+const LEFT = 37
+const RIGHT = 39
+const ESC = 27
 
 export default class WordCloudModal extends Component {
 
@@ -11,12 +16,26 @@ export default class WordCloudModal extends Component {
     // Make background not scrollable
     const body = document.getElementsByTagName('body')[0]
     body.className += 'modal-open'
+    // Add keydown event lister for interact with modal
+    document.addEventListener('keydown', this.handleKeyDown)
   }
 
   componentWillUnmount() {
     // Make background scrollable again
     const body = document.getElementsByTagName('body')[0]
     body.className = body.className.replace('modal-open', '')
+    // Remove keydown event lister
+    document.removeEventListener('keydown', this.handleKeyDown)
+  }
+
+  handleKeyDown = (e) => {
+    if (e.keyCode === ESC) {
+      browserHistory.push('/')
+    } else if (this.props.prevWordCloud && e.keyCode === LEFT) {
+      browserHistory.push(`/clouds/${this.props.prevWordCloud.name}`)
+    } else if (this.props.nextWordCloud && e.keyCode === RIGHT) {
+      browserHistory.push(`/clouds/${this.props.nextWordCloud.name}`)
+    }
   }
 
   render() {
