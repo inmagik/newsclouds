4ab63e6986e257d6b7d844c42d5728d510ce9e55
lorenzobianchi <lorenzo.bianchi@inmagik.com>
2016-12-06 17:23:46 +0100

    responsive layout

--- src/components/WordCloudModal/WordCloudModal.js
+++ src/components/WordCloudModal/WordCloudModal.js
@@ -45,11 +45,11 @@ export default class WordCloudModal extends Component {
         return (
             <div className="WordCloudModal">
                 <div className="WordCloudModal-content">
-                    <img src={inmagik} className="ModalLogo-inmagik pull-left"/> {/*<h5 className="ModalDate pull-left"><i className="fa fa-file-text"/> {formatDate(wordCloud.date)}</h5>*/}
-                    <img src={logo} className="ModalLogo-DailyClouds pull-left"/>
+                    {/*<img src={inmagik} className="ModalLogo-inmagik pull-left"/>
                     <h3 className="CloseButton"><Link to='/'><i className="fa fa-close"/></Link></h3>
                     <h3 className="ModalTitle">{formatDate(wordCloud.date)}</h3>
-                    <img width="100%" src={wordCloud.image} className="Modal-image"/> {prevWordCloud && <Link className="WordCloudModal-arrow-left" to={`/clouds/${prevWordCloud.name}`}>
+                    <img width="100%" src={wordCloud.image} className="Modal-image"/>
+                    {prevWordCloud && <Link className="WordCloudModal-arrow-left" to={`/clouds/${prevWordCloud.name}`}>
                         <h3><i className="fa fa-chevron-left"/></h3>
                     </Link>}
                     {nextWordCloud && <Link className="WordCloudModal-arrow-right" to={`/clouds/${nextWordCloud.name}`}>
@@ -59,6 +59,26 @@ export default class WordCloudModal extends Component {
                         <button><i className="fa fa-twitter"/></button>
                         <button><i className="fa fa-facebook"/></button>
                         <button><i className="fa fa-cloud-download"/></button>
+                    </div>
+
+                    */}
+                    <div className="modal-top">
+                      <h3 className="CloseButton"><Link to='/'><i className="fa fa-close"/></Link></h3>
+                      <h3 className="ModalTitle">{formatDate(wordCloud.date)}</h3>
+                    </div>
+                    <div className="modal-middle">
+                      <img width="100%" src={wordCloud.image} className="Modal-image img-responsive"/>
+                      {prevWordCloud && <Link className="WordCloudModal-arrow-left" to={`/clouds/${prevWordCloud.name}`}>
+                          <h3><i className="fa fa-chevron-left"/></h3>
+                      </Link>}
+                      {nextWordCloud && <Link className="WordCloudModal-arrow-right" to={`/clouds/${nextWordCloud.name}`}>
+                          <h3><i className="fa fa-chevron-right"/></h3>
+                      </Link>}
+                    <div className="SocialButtons">
+                        <button><i className="fa fa-twitter"/></button>
+                        <button><i className="fa fa-facebook"/></button>
+                        <button><i className="fa fa-cloud-download"/></button>
+                    </div>
                     </div>
                 </div>
             </div>
